09735b718b2a1ed64a7bfd305cd890ea60e88f42
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-10 09:19:36 -0300

    Criando Model

---
+++ FarmaciaAPI/Model/Produto.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FarmaciaAPI.Model
+{
+    public class Produto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string Nome { get; set; } = string.Empty;
+        
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string Marca { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Descricao { get; set; } = string.Empty;       
+
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Preco { get; set; }
+
+       
+    }
+}
