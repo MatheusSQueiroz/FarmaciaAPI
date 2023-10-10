3943148f34765ded0c97519fd210acb9f3de24ca
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-10 09:03:25 -0300

    Cofigurando projeto e criando camada Data

---
+++ FarmaciaAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FarmaciaAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
